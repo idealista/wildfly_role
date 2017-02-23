3aa089043952ac882b5aacdf04609581f7b0a107
javier monterrubio <jmonterrubio@idealista.com>
2017-02-23 14:40:58 +0100

    [PLATFORM-209] Update tests and idempotence

--- tests/test_ansible.py
+++ tests/test_ansible.py
@@ -11,23 +11,25 @@ def AnsibleVars(Ansible):
     return Ansible("include_vars", "tests/group_vars/group01.yml")["ansible_facts"]
 
 
-def test_wildfly_version(File, AnsibleVars):
+def test_wildfly_version(File, AnsibleVars, AnsibleDefaults):
     version = AnsibleVars["wildfly_version"]
-    assert File("/opt/wildfly-" + version).exists
-    assert File("/opt/wildfly").is_symlink
-    assert File("/opt/wildfly").linked_to == "/opt/wildfly-" + version
+    install_path = AnsibleDefaults["wildfly_install_path"]
+    assert File(install_path + "/wildfly-" + version).exists
+    assert File(install_path + "/wildfly").is_symlink
+    assert File(install_path + "/wildfly").linked_to == install_path + "/wildfly-" + version
 
 
-def test_wildfly_service(File, Service, Socket, AnsibleVars):
-    # port = AnsibleVars["wildfly_port"]
-    assert File("/etc/systemd/system/wildfly.service").exists
+def test_wildfly_service(File, Service, Socket, AnsibleDefaults):
+    assert File("/lib/systemd/system/wildfly.service").exists
     assert Service("wildfly").is_enabled
     assert Service("wildfly").is_running
-    # assert Socket("tcp://:::" + str(port)).is_listening
+    assert Socket("tcp://" + AnsibleDefaults["wildfly_bind"] + ":8080").is_listening
 
 
-def test_wildfly_user_creation(User, AnsibleVars):
-    assert User("wildfly").group == "wildfly"
-    assert User("wildfly").home == "/home/wildfly"
-    assert User("wildfly").shell == "/bin/sh"
-    assert User("wildfly").uid == 1001
+def test_wildfly_user_creation(User, Group, AnsibleVars):
+    user = AnsibleVars["wildfly_user"]
+    assert User(user).exists
+    assert Group(AnsibleVars["wildfly_group"]).exists
+    assert User(user).home == AnsibleVars["wildfly_user_home"]
+    assert User(user).shell == AnsibleVars["wildfly_user_shell"]
+    assert User(user).uid == AnsibleVars["wildfly_user_uid"]
